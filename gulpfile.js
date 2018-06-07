7847710d9a35597d3620cef45d63ada67a543336
root <javzero@hotmail.com>
2018-06-06 22:29:46 -0300

    Changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,38 @@
 const gulp = require('gulp'),
-sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer');
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('sass', ()=> 
-    gulp.src('./sass/*.sass')
+    gulp.src('./dev/sass/*.sass')
     .pipe(sass({
         outputStyle: 'nested', 
         sourceComments: false // Comments in the css where the property is in sass
-    }).on('err', sass.logError))
+    }).on('error', sass.logError))
     .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
     }))
-     .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./dist/css'))
 );
+
+gulp.task('pug', ()=> 
+    gulp.src('./dev/views/**/*.pug')
+        .pipe(plumber())
+        .pipe(pug({
+            pretty: true,
+            basedir: __dirname + '/dev/views/'
+        }))
+        .pipe(gulp.dest('./dist'))
+);
+
 gulp.task('default', () => {
-    gulp.watch('./sass/*.sass', ['sass'])
+    browserSync.init({
+        server: './dist'
+    });
+    gulp.watch('./dev/sass/*.sass', ['sass']).on('change', browserSync.reload);
+    gulp.watch('./dev/views/**/*.pug', ['pug']).on('change', browserSync.reload);
+    gulp.watch('./dist/js/**/*.js', ['pug']).on('change', browserSync.reload);
 });
--- theme-docs/Documentation.md
+++ theme-docs/Documentation.md
@@ -4,3 +4,29 @@
 ### Change Nav links to left
 If you want the nav buttons aligned to the left, next to the brand logo just add this class **".ml-auto"** right after the **.navbar-nav** class in the UL element. Example: "navbar-nav ml-auto"
 
+
+## Folder Structure
+
+/dist
+    - css (Styles)
+    - bootstrap-components (All boostrap components)
+    - img
+    - vendor (Plugins)
+    - js (Template scripts)
+    index.html
+/dev
+    - bootstrap-components (All boostrap components)
+    - includes (Partials of template: head - nav - scripts)
+    master.pug (Base template)
+    index.pug (Main page)
+
+## Functional Ajax Contact form 
+Contact form is functional. Uses form-sender.php file to process form.
+You only have to add the target email.
+
+
+## Development tools
+We use Sass as Css preprocessor and Pug as template preprocessor.
+You can find a gulpfile.js configured to work nicely with this tools.
+Also includes error handling via plumber.
+The pug templates are structured with includes, master views, etc.
